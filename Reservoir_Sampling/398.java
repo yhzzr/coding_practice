a4461d3abfc9fb6a5c2ab9574ac7182c346a0d9d
yhzzr <metagross@Hengs-MacBook-Pro.local>
2017-10-06 18:41:04 -0700

    Reservor Sampling related questions

---
+++ Reservoir_Sampling/398.java
@@ -0,0 +1,32 @@
+public class Solution {
+    int[] nums;
+    Random rnd;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rnd = new Random();
+    }
+    
+    public int pick(int target) {
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target)
+                continue;
+            else{
+                if (rnd.nextInt(count+1) == 0){
+                    result = i;
+                }
+                count++;
+            }
+        }
+        
+        return result;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
